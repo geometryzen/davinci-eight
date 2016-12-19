6179aa15d1d8186cea424ee01f614c87b6cb007a
David Holmes <david.geo.holmes@gmail.com>
2016-12-19 06:02:09 -0500

    tests

--- src/davinci-eight/geometries/SphereGeometry.spec.ts
+++ src/davinci-eight/geometries/SphereGeometry.spec.ts
@@ -1,6 +1,6 @@
-import { Engine } from '../core/Engine';
-import SphereGeometryOptions from './SphereGeometryOptions';
-import SphereGeometry from './SphereGeometry';
+// import Engine from '../core/Engine';
+// import SphereGeometryOptions from './SphereGeometryOptions';
+// import SphereGeometry from './SphereGeometry';
 // import Spinor3 from '../math/Spinor3';
 // import Vector3 from '../math/Vector3';
 
@@ -8,78 +8,5 @@ import SphereGeometry from './SphereGeometry';
 // const e3 = Vector3.vector(0, 0, 1);
 
 describe("SphereGeometry", function () {
-    describe("constructor", function () {
-        describe("radius property", function () {
-            it("should default to unity", function () {
-                const engine = new Engine();
-                const sphere = new SphereGeometry(engine);
-                expect(sphere.radius).toBe(1);
-                sphere.release();
-                engine.release();
-            });
-            it("should be mutable", function () {
-                const engine = new Engine();
-                const sphere = new SphereGeometry(engine);
-                sphere.radius = 7;
-                expect(sphere.radius).toBe(7);
-                sphere.release();
-                engine.release();
-            });
-        });
-        describe("getPrincipalScale", function () {
-            it("radius", function () {
-                const engine = new Engine();
-                const sphere = new SphereGeometry(engine);
-                expect(sphere.getPrincipalScale('radius')).toBe(1);
-                sphere.release();
-                engine.release();
-            });
-        });
-        describe("setPrincipalScale", function () {
-            it("radius", function () {
-                const engine = new Engine();
-                const sphere = new SphereGeometry(engine);
-                sphere.setPrincipalScale('radius', 2);
-                expect(sphere.getPrincipalScale('radius')).toBe(2);
-                sphere.release();
-                engine.release();
-            });
-        });
-        describe("scaling", function () {
-            it("radius", function () {
-                const engine = new Engine();
-                const sphere = new SphereGeometry(engine);
-                const scaling = sphere.scaling;
-                expect(scaling.getElement(0, 0)).toBe(1);
-                sphere.release();
-                engine.release();
-            });
-            it("radius should be in all diagonal elements", function () {
-                const engine = new Engine();
-                const options: SphereGeometryOptions = {};
-                // options.tilt = Spinor3.one();
-                const sphere = new SphereGeometry(engine, options);
-                sphere.setPrincipalScale('radius', 5);
-                const scaling = sphere.scaling;
-                expect(scaling.getElement(0, 0)).toBe(5);
-                expect(scaling.getElement(1, 1)).toBe(5);
-                expect(scaling.getElement(2, 2)).toBe(5);
-                sphere.release();
-                engine.release();
-            });
-            it("radius should be in all diagonal elements", function () {
-                const engine = new Engine();
-                const options: SphereGeometryOptions = {};
-                // options.tilt = Spinor3.rotorFromDirections(e2, e3)code;
-                const sphere = new SphereGeometry(engine, options);
-                sphere.setPrincipalScale('radius', 5);
-                const scaling = sphere.scaling;
-                expect(scaling.getElement(0, 0)).toBe(5);
-                expect(scaling.getElement(1, 1)).toBe(5);
-                expect(scaling.getElement(2, 2)).toBe(5);
-                sphere.release();
-                engine.release();
-            });
-        });
-    });
+    // TODO
 });
